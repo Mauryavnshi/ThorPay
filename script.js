ddfe2af0d17bf821bed8aa7d871550d1eb42aa49
Prashant Maurya <128934790+Mauryavnshi@users.noreply.github.com>
2026-07-15 13:38:27 +0530

    Add wallet button interaction

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("ThorPay Loaded");
+const connectButton = document.querySelector("button");
+
+connectButton.addEventListener("click", () => {
+    alert("Wallet connection feature coming soon on ThorPay!");
+});

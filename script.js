f06faf8b5cb37de754eb54a45e3d92563c5b438b
Prashant Maurya <128934790+Mauryavnshi@users.noreply.github.com>
2026-07-16 12:36:48 +0530

    Improve UI with Arc faucet, roadmap and payment status

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const connectButton = document.querySelector("button");
 connectButton.addEventListener("click", () => {
     alert("Wallet connection feature coming soon on ThorPay!");
 });
+
 const sendBtn = document.getElementById("sendBtn");
 
 sendBtn.addEventListener("click", () => {
-  alert("Demo payment sent successfully!");
+    document.getElementById("status").innerText =
+        "Status: Demo Payment Successful ✅";
+
+    alert("Demo payment sent successfully!");
 });
